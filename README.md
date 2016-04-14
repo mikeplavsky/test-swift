120a7f0e374783003be00b81432c8c14bd30a5b3
mikeplavsky <mike.plavsky@quest.com>
2016-04-14 10:36:24 -0400

    how to run on AWS Lambda

---
+++ README.md
@@ -0,0 +1,2 @@
+To run on AWS 
+https://medium.com/@gigq/using-swift-in-aws-lambda-6e2a67a27e03#.d1gz5ybgf 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,8 @@
 say("Yep, Man, \(Process.arguments[1])")
 
 let x: Int -> ((Int) -> String) = {x in 
-    {y in "Got \(x) and \(y)"}}
+    {y in "Got \(x) and \(y)"}
+}
 
 let i1 = Int(Process.arguments[1])!
 let i2 = Int(Process.arguments[2])!
