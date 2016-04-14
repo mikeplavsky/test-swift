7d9e5a1a3ae09b9d45185ace8c0fa65f90072bdb
mikeplavsky <mike.plavsky@quest.com>
2016-04-14 10:27:49 -0400

    lambdas

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,11 @@
 say("Yep, Man, \(Process.arguments[1])")
+
+let x: Int -> ((Int) -> String) = {x in 
+    {y in "Got \(x) and \(y)"}}
+
+let i1 = Int(Process.arguments[1])!
+let i2 = Int(Process.arguments[2])!
+
+print(
+    x(i1)(i2)
+)
